ed696a5e0a377e39759768aabff4c467cae5449a
Navod Dilshan <navoddilshan9@gmail.com>
2022-11-12 03:33:29 +0530

    s3 bucket aded

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const s3 = require('aws-sdk/clients/s3')
+const express = require('express')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const { uploadFile } = require('./s3')
+const app = express()
 
-//upload a file to s3
+app.post('/images', upload.single('image'), async (req, res, next) => {
+  const file = req.file
+  console.log(file)
+  const result = await uploadFile(file)
+  console.log(result)
 
-//download a fle from s3
+  res.send('ad')
+})
+
+app.listen(8080, () => console.log('liestening on port 8080'))
